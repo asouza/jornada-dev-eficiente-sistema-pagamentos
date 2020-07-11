4de92614e7215feca5902c70187e016bafe6331c
alberto souza <alots.ssa@gmail.com>
2020-07-11 19:16:54 -0300

    comeco do projeto

---
+++ src/main/java/com/deveficiente/pagamentos/DesafioSistemaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSistemaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSistemaPagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/pagamentos/DesafioSistemaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSistemaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
