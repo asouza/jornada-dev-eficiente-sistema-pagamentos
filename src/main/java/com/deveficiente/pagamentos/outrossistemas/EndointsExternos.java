8c76f260e8d5fba053d1b9b64f5cd032f7c77952
alberto souza <alots.ssa@gmail.com>
2020-07-12 17:03:57 -0300

    sistema externo da um notfound para id de pedido de vez em quando

--- src/main/java/com/deveficiente/pagamentos/outrossistemas/EndointsExternos.java
+++ src/main/java/com/deveficiente/pagamentos/outrossistemas/EndointsExternos.java
@@ -3,16 +3,24 @@ package com.deveficiente.pagamentos.outrossistemas;
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EndointsExternos {
 	
+	private AtomicInteger ids = new AtomicInteger();
 
 	@GetMapping("/api/pedidos/{idPedido}/valor")
 	public Map<String, Object> valorPedido(Long idPedido) {
+		if(ids.getAndIncrement() % 3 == 0) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
 		String[] valores = new String[]{"50.00", "150.00","60","200"};
 		int posicao = new Random().nextInt(4);
 		return Map.of("valor",new BigDecimal(valores[posicao])); 
